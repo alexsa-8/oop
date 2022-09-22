81ed81bfe7f6c9caf6eed3dab6fa9f97def423cc
Alex <alexsa-8@mail.ru>
2022-09-22 23:34:55 +0300

    oop 2 задание

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,12 @@ public class Main {
         human maxim = new human("Максим", "Минск", 2022 - 35);
         human anay = new human("Аня", "Москва", 2022 - 29);
         human katay = new human("Катя", "Калининград", 2022 - 28);
-        human artem = new human("Максим", "Москва", 2022 - 27);
+        human artem = new human("Артём", "Москва", 2022 - 27);
+
+        human maxim1 = new human("Максим", "Минск", 35, "бренд-менеджер");
+        human anay1 = new human("Аня", "Москва", 29, "методист образовательных программ");
+        human katay1 = new human("Катя", "Калининград", 28, "продакт-менеджер");
+        human artem1 = new human("Артём", "Москва", 27, "директор по развитию бизнеса");
 
     }
 }
--- src/human.java
+++ src/human.java
@@ -2,9 +2,16 @@ public class human {
     String name;
     String town;
     int yearOfBirth;
+    String job;
 
     public human(String name, String town, int yearOfBirth) {
-        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году");
+        System.out.println("Привет! Меня зовут " + name + ". Я из города "
+                + town + ". Я родился в " + yearOfBirth + " году. Будем знакомы!");
+    }
+
+    public human(String name, String town, int yearOfBirth, String job) {
+        System.out.println("Привет! Меня зовут " + name + ". Я из города "
+                + town + ". Мне " + yearOfBirth + " лет. Я работаю на должности " + job + ". Будем знакомы!");
     }
 
 }
