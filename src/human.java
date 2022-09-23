a53e45669b1de98dadbf125c13fdf032172c03e1
Alex <alexsa-8@mail.ru>
2022-09-23 17:48:27 +0300

    oop использован конструктор

--- src/human.java
+++ src/human.java
@@ -10,6 +10,10 @@ public class human {
     }
 
     public human(String name, String town, int yearOfBirth, String job) {
+        this.name=name;
+        this.town=town;
+        this.yearOfBirth=yearOfBirth;
+        this.job=job;
         System.out.println("Привет! Меня зовут " + name + ". Я из города "
                 + town + ". Мне " + yearOfBirth + " лет. Я работаю на должности " + job + ". Будем знакомы!");
     }
