4dff91e0bee22a074b77f58b94b94cbdfdd0da20
Alex <alexsa-8@mail.ru>
2022-09-27 20:50:06 +0300

    Инкапсуляция Домашнее задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,57 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
-        human maxim = new human("Максим", "Минск", 2022 - 35);
-        human anay = new human("Аня", "Москва", 2022 - 29);
-        human katay = new human("Катя", "Калининград", 2022 - 28);
-        human artem = new human("Артём", "Москва", 2022 - 27);
-        System.out.println();
-        human maxim1 = new human("Максим", "Минск", -35, "бренд-менеджер");
-        human anay1 = new human("Аня", null, 29, "методист образовательных программ");
-        human katay1 = new human(null, "Калининград", 28, "продакт-менеджер");
-        human artem1 = new human("Артём", "Москва", 27, null);
-        maxim1.messagePerson();
-        anay1.messagePerson();
-        katay1.messagePerson();
-        artem1.messagePerson();
-
+        human maxim = new human("Максим", "Минск", -35, "бренд-менеджер");
+        human anay = new human("Аня", null, 29, "методист образовательных программ");
+        human katay = new human(null, "Калининград", 28, "продакт-менеджер");
+        human artem = new human("Артём", "Москва", 27, null);
+        human vladimir = new human("Владимир", "Казань", 21, "не работаю");
+        maxim.messagePerson();
+        anay.messagePerson();
+        katay.messagePerson();
+        artem.messagePerson();
+        vladimir.messagePerson();
+        flower roza = new flower("Роза", null, "Голландия", 35.59, 0);
+        flower hrizantema = new flower("Хризантема", null, null, 15, 5);
+        flower pion = new flower("Пион", null, "Англия", 69.9, 1);
+        flower gipsofila = new flower("Гипсофила", null, "Турция", 19.9, 10);
+        roza.outputInformationFlower();
+        hrizantema.outputInformationFlower();
+        pion.outputInformationFlower();
+        gipsofila.outputInformationFlower();
+        int[] bouquet=new int[4];
+        int numberRoza = 0;
+        if (numberRoza>0){
+            bouquet[0]=roza.getLifeSpan();
+        }
+        int numberHrizantema = 5;
+        if (numberHrizantema>0){
+            bouquet[1]=hrizantema.getLifeSpan();
+        }
+        int numberPion = 0;
+        if (numberPion>0){
+            bouquet[2]=pion.getLifeSpan();
+        }
+        int numberGipsofila = 4;
+        if (numberGipsofila>0){
+            bouquet[3]=gipsofila.getLifeSpan();
+        }
+        Arrays.sort(bouquet);
+        int day=0;
+        for (int j : bouquet) {
+            if (j > 0) {
+                day = j;
+                break;
+            }
+        }
+        double Bouquet = (roza.getCost() * numberRoza + hrizantema.getCost() * numberHrizantema +
+                pion.getCost() * numberPion + gipsofila.getCost() * numberGipsofila) * 1.1;
+        System.out.print("Букет из: " + roza.nameFlower + " - " + numberRoza + ", "
+                + hrizantema.nameFlower + " - " + numberHrizantema + ", "
+                + pion.nameFlower + " - " + numberPion + ", "
+                + gipsofila.nameFlower + " - " +numberGipsofila+" стоит: ");
+        System.out.printf("%.2f", Bouquet);
+        System.out.println(" руб. Простоит "+day+" суток.");
     }
 }
--- src/human.java
+++ src/human.java
@@ -2,30 +2,41 @@ import java.util.Objects;
 
 public class human {
     String name;
-    String town;
-    int yearOfBirth;
+    private String town;
+    private int yearOfBirth;
     String job;
 
-    public human(String name, String town, int yearOfBirth) {
-        System.out.println("Привет! Меня зовут " + name + ". Я из города "
-                + town + ". Я родился в " + yearOfBirth + " году. Будем знакомы!");
+    human(String name, String town, int yearOfBirth, String job) {
+        //this.name = name;
+        setTown(town);
+        setYearOfBirth(yearOfBirth);
+        //this.job = job;
+        this.name = Objects.requireNonNullElse(name, "Информация не указана");
+        this.job = Objects.requireNonNullElse(job, "Информация не указана");
+
+    }
+    public String getTown() {
+        return town;
     }
 
-    human(String name, String town, int yearOfBirth, String job) {
-        this.name= Objects.requireNonNullElse(name,"Информация не указана");
-        this.town=Objects.requireNonNullElse(town,"Информация не указана");
-        this.job=Objects.requireNonNullElse(job,"Информация не указана");
-
-        if (yearOfBirth < 0) {
-            this.yearOfBirth = Math.abs(yearOfBirth);
+    public void setTown(String town) {
+        if (town == null || town.isEmpty()) {
+            this.town = "Информация не указана";
         } else {
-            this.yearOfBirth = yearOfBirth;
+            this.town = town;
         }
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = Math.max(yearOfBirth, 0);
     }
 
     void messagePerson() {
         System.out.println("Привет! Меня зовут " + name + ". Я из города "
                 + town + ". Мне " + yearOfBirth + " лет. Я работаю на должности " + job + ". Будем знакомы!");
     }
-
 }
